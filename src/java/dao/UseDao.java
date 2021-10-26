37d676623ccbbe5d1a7e542f4b3bb1c4bed6a880
Canwaiting <455021735@qq.com>
2021-10-26 13:32:56 +0800

    1

--- src/java/dao/UseDao.java
+++ src/java/dao/UseDao.java
@@ -1,11 +1,23 @@
 package dao;
 
+import java.net.ConnectException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class UseDao implements Dao {
     @Override
     public boolean login(User user) throws Exception {
-        if(user.name.equals()){
-            return true;
-        }
+        //初始化数据
+        String sql = "select * from user where name=?";
+
+        //创建连接
+        Connection conn = DBConnection.getConnection();
+        PreparedStatement preStmt = conn.prepareStatement(sql);
+        //查询 比对
+        //关闭连接
+//        if(user.name.equals()){
+//            return true;
+//        }
         return false;
     }
 }
